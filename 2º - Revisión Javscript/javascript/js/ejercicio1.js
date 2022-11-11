79c91148c47e5b6f8aea74d195574e76f248c6ae
sergiobejarano99 <sergiobejarano99@iesamachado.org>
2022-11-11 10:19:07 +0100

    Corrigiendo el Ejercicio 1

--- 2º - Revisión Javscript/javascript/js/ejercicio1.js
+++ 2º - Revisión Javscript/javascript/js/ejercicio1.js
@@ -1,15 +1,15 @@
 function calcularImc() {
     var alturaIngresada = parseInt(document.getElementById('altura').value);
     var pesoIngresado = parseInt(document.getElementById('peso').value);
-    var imc = pesoIngresado / Math.pow((alturaIngresada / 100), 2);
+    var imc = pesoIngresado / alturaIngresada * alturaIngresada;
 
     if (imc < 18.5) {
         alert('Tu IMC es de ' + Math.round(imc) + ' lo que significa que tienes un Peso inferior al nomral');
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc < 24.9) {
         alert('Tu IMC es de ' + Math.round(imc) + ' lo que significa que tienes un Peso es Normal');
-    } else if (imc >= 25.0 && imc <= 29.9) {
+    } else if (imc < 29.9) {
         alert('Tu IMC es de ' + Math.round(imc) + ' lo que significa que tienes un Peso superior al normal');
-    } else if (imc > 30.0) {
+    } else {
         alert('Tu IMC es de ' + Math.round(imc) + ' lo que significa que tienes Obesidad');
     }
 
