c3a4e1949e43d4156a4fbf279dec429354ac8500
sergiobejarano99 <sergiobejarano99@iesamachado.org>
2022-11-11 12:28:45 +0100

    Intentado ejercicio 3

--- 2º - Revisión Javscript/javascript/js/ejercicio2.js
+++ 2º - Revisión Javscript/javascript/js/ejercicio2.js
@@ -1,7 +1,13 @@
 function enviarMensaje() {
 
 }
-
+/*
+function imprimePost() {
+    let longitud=posts.lenght();
+    let i=0, codigoHtml();
+    let icono="<div class>";
+}
+*/
 
 
 
--- 2º - Revisión Javscript/javascript/js/ejercicio3.js
+++ 2º - Revisión Javscript/javascript/js/ejercicio3.js
@@ -1,5 +1,15 @@
 function crearUsuario() {
+    var validarNombre = document.getElementById('nombre').value;
+    var validarApellido = document.getElementById('apellido').value;
+    var validarEmail = document.getElementById('email').value;
+    var validarDni = document.getElementById('dni').value;
+    var validarPin = document.getElementById('pin').value;
+    var validarRepetirPin = document.getElementById('repetirPin').value;
+    var enviarFormulario = document.getElementById('enviarFormulario').value;
 
+    if (validarNombre.lenght == "") {
+        alert("falta");
+    }
 }
 
 
