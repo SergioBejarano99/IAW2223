6498efb590ec309da68f444f492cfab6f7b013d4
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2022-11-12 18:18:06 +0100

    Ejercicio 2

--- 2º - Revisión Javscript/javascript/js/ejercicio3.js
+++ 2º - Revisión Javscript/javascript/js/ejercicio3.js
@@ -1,14 +1,27 @@
 function crearUsuario() {
-    var validarNombre = document.getElementById('nombre').value;
-    var validarApellido = document.getElementById('apellido').value;
-    var validarEmail = document.getElementById('email').value;
-    var validarDni = document.getElementById('dni').value;
-    var validarPin = document.getElementById('pin').value;
-    var validarRepetirPin = document.getElementById('repetirPin').value;
+    var nombreFormulario = document.getElementById('nombre').value; //Validado.
+    var apellidoFormulario = document.getElementById('apellido').value;  //Validado
+    var emailFormulario = document.getElementById('email').value;
+    var telefonoFormulario = document.getElementById('telefono').value;
+    var dniFormulario = document.getElementById('dni').value;
+    var pinFormulario = document.getElementById('pin').value;
+    var repetirPinFormulario = document.getElementById('repetirPin').value;
     var enviarFormulario = document.getElementById('enviarFormulario').value;
 
-    if (validarNombre.lenght == "") {
-        alert("falta");
+    if (nombreFormulario == null || nombreFormulario.length == 0) {
+        document.getElementById('faltaNombre').innerHTML = "Debes introudcir tu Nombre.";
+    } else if (apellidoFormulario == null || apellidoFormulario.length == 0) {
+        document.getElementById('faltaApellido').innerHTML = "Debes introducir al menos un Apellido.";
+    } else if (emailFormulario == null || emailFormulario.length == 0) {
+        document.getElementById('faltaEmail').innerHTML = "Debes introducir una Dirección de Correo Electrónico Válida.";
+    } else if (dniFormulario == null || dniFormulario.length == 0) {
+        document.getElementById('faltaDni').innerHTML = "El DNI que has intrdoucido no es Válido.";
+    } else if (!(/^\d{9}$/.test(pinFormularrio))) {
+        document.getElementById('faltaPin').innerHTML = "El PIN debe ser de 8 Dígitos.";
+    } else if (pinFormulario.length != repetirPinFormulario.length) {
+        document.getElementById('faltaRepetirPin').innerHTML = "El PIN no coincide";
+    } else {
+        alert(nombreFormulario.substring(0, 2) + apellidoFormulario.substring(0, 2) + telefonoFormulario.substring(10, 6));
     }
 }
 
