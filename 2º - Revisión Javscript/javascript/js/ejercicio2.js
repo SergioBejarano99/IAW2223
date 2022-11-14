800f608206361fc3ceae370ca654dd152c8a402f
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2022-11-14 19:45:28 +0100

    Ejercicio 2 casi terminado

--- 2º - Revisión Javscript/javascript/js/ejercicio2.js
+++ 2º - Revisión Javscript/javascript/js/ejercicio2.js
@@ -1,13 +1,42 @@
-function enviarMensaje() {
+let mensajes = [];
+let comprobarMensajeVacio = false;
+let posts = [];
+let sinMensaje = false;
 
+imprimePosts();
+function ordenar() {
+    posts.sort();
+    imprimePosts();
 }
-/*
-function imprimePost() {
-    let longitud=posts.lenght();
-    let i=0, codigoHtml();
-    let icono="<div class>";
+
+function eliminar(elemento) {
+    let pos = posts.indexOf(elemento);
+    posts.splice(pos, 1);
+    imprimePosts();
 }
-*/
+
+function anadir() {
+    var mensaje = document.getElementById("post").value;
+    posts.push(mensaje);
+    imprimePosts();
+}
+
+function imprimePosts() {
+    var mensajeIntroduciVacio = document.getElementById('post').value;
+    if (mensajeIntroduciVacio.length == "") {
+        alert("Debes introducir un Mensaje.");
+        sinMensaje = false;
+    } else {
+        let longitud = posts.length;
+        let i = 0, codigoHTML = "";
+        let icono = "<div class='tweet'>";
+        for (i = 0; i < longitud; i++) {
+            codigoHTML += icono + posts[i] + " <button class=\"btn-image\" onclick='eliminar(posts[" + i + "]);''></button><br></div>";
+        }
+        document.getElementById("demo").innerHTML = codigoHTML;
+    }
+}
+
 
 
 
