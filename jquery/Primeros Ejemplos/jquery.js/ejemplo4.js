300a7f95476b9b8c969ed6356a43cc012c192d56
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2022-11-18 19:41:27 +0100

    Cuarto Ejemplo con jQuery

---
+++ jquery/Primeros Ejemplos/jquery.js/ejemplo4.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#cssOriginal").click(function (cssOriginal) {
+        cssOriginal.preventDefault();
+        $("#primerParrafo").css("color", "black");
+        $("#primerParrafo").css("font-size", "100%");
+        $("#segundoParrafo").css("color", "black");
+        $("#segundoParrafo").css("font-size", "100%");
+        $("#tercerParrafo").css("color", "black");
+        $("#tercerParrafo").css("font-size", "100%");
+        $("#cuartoParrafo").css("color", "black");
+        $("#cuartoParrafo").css("font-size", "100%");
+    });
+
+    $("#cssModificado").click(function (cssOriginal) {
+        cssOriginal.preventDefault();
+        $(".parrafoRojo").css({ "color": "red", "font-size": "200%" });
+        $(".parrafoAzul").css({ "color": "blue", "font-size": "200%" });
+    });
+});
+
+
+
+
+
+/* © Sergio Bejarano Arroyo */
