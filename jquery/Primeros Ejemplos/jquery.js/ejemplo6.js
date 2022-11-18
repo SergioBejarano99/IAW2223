2788ed6ea04b3a1c42f43c36403be183e5821ba1
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2022-11-18 20:48:03 +0100

    Sexto Ejemplo con jQuery

--- jquery/Primeros Ejemplos/jquery.js/ejemplo6.js
+++ jquery/Primeros Ejemplos/jquery.js/ejemplo6.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
     $("#anadirEstilosCss").click(function (e) {
         e.preventDefault();
-<<<<<<< HEAD
         $("tr:even").addClass("fondoAzul");
         $("tr:odd").addClass("fondoVerde");
-=======
-        $("tr:even").addClass("fondosRojos");
-        $("tr:odd").addClass("fondosAmarillos");
->>>>>>> 75308f94a0bb042fb08f18f9a7124b1b5bda90b4
     });
 
     $("#eliminarEstilosCss").click(function (e) {
