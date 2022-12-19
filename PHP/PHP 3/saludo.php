f3d4568ea3deb250c8d8a3c2bb2f61c2b9da0dc8
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2022-12-19 19:45:42 +0100

    Ejercicio 1

--- PHP/PHP 3/saludo.php
+++ PHP/PHP 3/saludo.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1</title>
+
+    <meta autor="Sergio Bejarano Arroyo" />
 </head>
+
 <body>
     <h1>SALUDO</h1>
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label>Introduce tun Nombre:</label>
         <input type="text" name="nombre" placeholder="Escribe tu nombre">
-        <input type="button" value="Enviar">
+        <input type="submit" value="Enviar">
     </form>
 
     <?php
@@ -24,4 +28,5 @@
     }
     ?>
 </body>
+
 </html>
