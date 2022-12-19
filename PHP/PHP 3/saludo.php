118dacd82b8a8a4ac758d1c8b7ef0fe81ddb0d75
sergiobejarano99 <sergiobejarano99@iesamachado.org>
2022-12-19 14:26:59 +0100

    HOY

---
+++ PHP/PHP 3/saludo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1>SALUDO</h1>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <label>Introduce tun Nombre:</label>
+        <input type="text" name="nombre" placeholder="Escribe tu nombre">
+        <input type="button" value="Enviar">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombreIntroducido = htmlspecialchars($_POST['nombre']);
+        $fechaActual = date('d/m/y');
+
+        echo '<p>Hola ' . $nombreIntroducido . ' hoy es ' . $fechaActual . '</p>';
+    }
+    ?>
+</body>
+</html>
