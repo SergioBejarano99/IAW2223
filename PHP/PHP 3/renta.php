c48d9b9660c70a50a5b6356cf3771b58e8dc9f0c
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2023-01-09 20:25:28 +0100

    Ejercicio 7

--- PHP/PHP 3/renta.php
+++ PHP/PHP 3/renta.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <h1>RENTA:</h1>
+    <h1>RENTA</h1>
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="nombre">Nombre:</label>
