eaca332ec304dee123301d0b6204f984fea99a61
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2023-01-09 20:38:07 +0100

    Ejercicio 7

--- PHP/PHP 3/rentareto.php
+++ PHP/PHP 3/rentareto.php
@@ -13,57 +13,54 @@
 <body>
     <h1>RENTA RETO</h1>
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" placeholder="Escribe tu Nombre."><br>
-    <label for="apellido">Apellido:</label>
-    <input type="text" name="apellido" placeholder="Escribe tus Apellidos."><br>
-    <label for="email">Email:</label>
-    <input type="email" name="email" placeholder="Escribe tu Email."><br>
-    <label for="bruto">Salario Bruto:</label>
-    <input type="number" name="bruto" min="1"><br>
-    <label for="ong">Colabora con ONGs</label><input type="checkbox" name="ong"><br>
-    <input type="submit" value="Calcular"><br>
-</form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" placeholder="Escribe tu Nombre."><br>
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" placeholder="Escribe tus Apellidos."><br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" placeholder="Escribe tu Email."><br>
+        <label for="bruto">Salario Bruto:</label>
+        <input type="number" name="bruto" min="1"><br>
+        <label for="ong">Colabora con ONGs</label><input type="checkbox" name="ong"><br>
+        <input type="submit" value="Calcular"><br>
+    </form>
 
-<?php
-    if ($_POST){
+    <?php
+    if ($_POST) {
         $bruto = htmlspecialchars($_POST["bruto"]);
-        //print_r($_POST);
-        if (isset($_POST["ong"]))
+
+        if (isset($_POST["ong"])) {
             $casilla = htmlspecialchars($_POST["ong"]);
-        else
-            $casilla = "off"; // Desmarcada, no ha sido enviada
-        $tipo = 0;
+        } else {
+            $casilla = "off";
+            $tipo = 0;
+        }
+
         if ($bruto <= 0) {
             echo "<p>Debe introducir su sueldo bruto y ser mayor que cero.";
-        }
-        elseif ($bruto < 10000) {
+        } elseif ($bruto < 10000) {
             $tipo = 0.05;
-        }
-        elseif ($bruto < 20000) {
+        } elseif ($bruto < 20000) {
             $tipo = 0.15;
-        }
-        elseif ($bruto < 35000) {
+        } elseif ($bruto < 35000) {
             $tipo = 0.2;
-        }
-        elseif ($bruto < 60000) {
+        } elseif ($bruto < 60000) {
             $tipo = 0.3;
-        }
-        else {
+        } else {
             $tipo = 0.45;
         }
-        echo "<p>Bruto: $bruto</p>"; $impositivo=$tipo*100;
-        echo "<p>Tipo impositivo aplicado: $impositivo%</p>";
-        if (strcmp($casilla,"off")==0)
-            $total = $bruto*$tipo;
-        else{
-            $total = $bruto*$tipo;
-            $total = $total - $total*0.02;
+
+        if (strcmp($casilla, "off") == 0) {
+            $total = $bruto * $tipo;
+        } else {
+            $total = $bruto * $tipo;
+            $total = $total - $total * 0.02;
         }
-        echo "<p>Debe pagar $total € en su declaración</p>";
+
+        echo '<p>La Cantidad que debe pagar, es de ' . $total . '€.</p>';
     }
-?>
+    ?>
 </body>
 
 </html>
