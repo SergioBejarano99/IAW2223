425ac29769f42b31ae45f5fa4a088a61fd080eaf
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2023-01-09 20:45:10 +0100

    Ejercicio 6

--- PHP/PHP 3/sortea2.php
+++ PHP/PHP 3/sortea2.php
@@ -8,45 +8,44 @@
     <title>Ejercicio 6</title>
 
     <meta autor="Sergio Bejarano Arroyo" />
-
-    <style>
-        input[name="premios"] {
-            width: 210px;
-        }
-
-        textarea {
-            width: 412px;
-            height: 200px;
-        }
-    </style>
 </head>
 
 <body>
     <h1>SORTEA 2</h1>
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <label>Participantes:</label><br>
-        <textarea name="participantes" placeholder="Escribe los Participantes, separados por Espacios."></textarea><br>
-        <label>Número de Premios:</label>
-        <input type="text" name="numeroPremios" placeholder="Introduce la Cantidad de Premios.">
-        <input type="submit" name="submit" value="Sortear">
+        <label for="participantes">Lista de participantes:</label><br>
+        <textarea name="participantes" id="participantes" cols="40" rows="10"></textarea><br>
+        <label for="numeropremios">Nº de premios:</label>
+        <input type="number" name="numeropremios"><br>
+        <input type="submit" value="¡Sortear!">
     </form>
+
     <?php
-    if (isset($_POST["submit"])) {
-        $cantidadParticipantes = htmlspecialchars($_POST["participantes"]);
-        $cantidadParticipantes = explode(" ", $cantidadParticipantes);
+    if ($_POST) {
+        $participantes = htmlspecialchars($_POST["participantes"]);
+        $numeropremios = htmlspecialchars($_POST["numeropremios"]);
 
-        echo "Los participantes son: ";
+        if (is_numeric($numeropremios) && $numeropremios >= 1 && round($numeropremios, 0) == $numeropremios) {
+            $agraciados = explode("\n", $participantes);
 
-        for ($j = 0; $j < count($cantidadParticipantes); $j++) {
-            echo $cantidadParticipantes[$j] . ", ";
-        }
+            if ($numeropremios < sizeof($agraciados)) {
+                $premios = array_rand($agraciados, $numeropremios);
 
-        $cantidadPremios = intval(htmlspecialchars($_POST["numeroPremios"]));
-
-        for ($i = 1; $i <= $cantidadPremios; $i++) {
-            $ganadaor = mt_rand(1, count($cantidadParticipantes));
-            echo "El premio " . $i . " es para: " . $cantidadParticipantes[$ganadaor] . "<br>";
+                if (is_array($premios)) {
+                    for ($indice = 0; $indice < sizeof($premios); $indice++) {
+                        $premio = $premios[$indice];
+                        echo "<p>$agraciados[$premio] ha sido premiado/a</p>";
+                    }
+                } else {
+                    $premiado = rand(0, sizeof($agraciados) - 1);
+                    echo "<p>$agraciados[$premiado] ha sido premiado/a</p>";
+                }
+            } else {
+                echo "<p>Hay más premios que participantes</p>";
+            }
+        } else {
+            echo "<p>Debe introducir un número positivo mayor que 1</p>";
         }
     }
     ?>
