f1fb5834c9cfb8fccda1405f6b5f6ed7f1026e26
SergioBejarano99 <sergiobejarano99@iesamachado.org>
2023-01-16 19:19:34 +0100

    webscrapping.php

--- PHP/PHP 4/webscrapping.php
+++ PHP/PHP 4/webscrapping.php
@@ -1,9 +1,42 @@
-<?php
-    echo "";
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>webscrapping.php</title>
 
+    <meta autor="Sergio Bejarano Arroyo" />
 
+    <style>
+        input[name="url"] {
+            width: 500px;
+        }
+    </style>
+</head>
 
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label>URL:</label>
+        <input type="text" name="url" placeholder="Escribe la URL."><br>
+        <input type="submit" value="Eniar">
+    </form>
 
-/* © Sergio Bejarano Arroyo */
-?>
+    <?php
+    $campoObligatorio = "";
+
+    $urlIntroducida = "";
+
+    if (isset($_POST["submit"])) {
+        if (empty($_POST["url"])) {
+            $campoObligatorio = "Debes introducir una URL.";
+        } else {
+            $html = file_get_contents($urlIntroducida);
+            echo $html;
+        }
+    }
+    ?>
+</body>
+
+</html>
